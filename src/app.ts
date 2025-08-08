bec50bad2b3e09ebe80f422c851f3a0d0f2f10d6
Emilia851 <emiliagalloalzate85@gmail.com>
2025-08-08 10:46:54 -0500

    quitar 3000

--- src/app.ts
+++ src/app.ts
@@ -79,9 +79,9 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ "ms": "Bienvenido a mi aplicación para probar las rutas en Swagger necesitas estar en entorno local por seguridad, si estas en producción puedes probar las mismas rutas en Postman" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`El server corre bien en http://localhost:${PORT}`);
+  console.log(`El server corre bien en ${PORT}`);
 });
 
 export default app;
